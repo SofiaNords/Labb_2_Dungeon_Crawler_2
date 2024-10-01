f1edd36d570aaec078049bacb85192b2b95b0664
SofiaNords <sofia.k.nordstrom.com>
2024-10-01 13:07:36 +0200

    Add project files.

---
+++ Labb_2_Dungeon_Crawler_2/LevelElement.cs
@@ -0,0 +1,11 @@
+﻿public abstract class LevelElement
+{
+    public virtual int X { get; set; }
+
+    public virtual int Y { get; set; }
+
+    public char Char { get; set; }
+
+    public ConsoleColor Color { get; set; }
+
+}
