a8e70e8b424c4475b2dfdd760a2de7d1b54f6cc8
SofiaNords <sofia.k.nordstrom.com>
2024-10-08 17:21:16 +0200

    Create Player class

---
+++ Labb_2_Dungeon_Crawler_2/Player.cs
@@ -0,0 +1,12 @@
+﻿public class Player : LevelElement
+{
+    public string Name { get; set; }
+
+    public int HP { get; set; }
+
+    public Player(int x, int y, char icon, ConsoleColor color) : base(x, y, icon, color)
+    {
+        Name = "Sofia";
+        HP = 100;
+    }
+}
