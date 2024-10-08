63b05d2d4f580cbac4010eae67c6d763a8702e56
SofiaNords <sofia.k.nordstrom.com>
2024-10-08 17:00:01 +0200

    Update LevelElement.cs

---
+++ Labb_2_Dungeon_Crawler_2/Elements/LevelElement.cs
@@ -0,0 +1,25 @@
+﻿public abstract class LevelElement
+{
+    public virtual int PositionX { get; set; }
+    public virtual int PositionY { get; set; }
+    protected char Icon { get; set; }
+    protected ConsoleColor Color { get; set; }
+
+    public LevelElement(int x, int y, char icon, ConsoleColor color)
+    {
+        PositionX = x;
+        PositionY = y;
+        Icon = icon;
+        Color = color;
+    }
+
+
+    public void Draw()
+    {
+        Console.ForegroundColor = Color;
+        Console.SetCursorPosition(PositionX, PositionY);
+        Console.Write(Icon);
+        Console.ResetColor();
+    }
+
+}
